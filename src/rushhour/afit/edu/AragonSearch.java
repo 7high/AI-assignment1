1a3f6fb944725ee8e92a1205e9629e866acbd8c3
7high <angelito.aragon.jr@gmail.com>
2018-01-09 09:47:50 -0500

    AI Assignment 1

---
+++ src/rushhour/afit/edu/AragonSearch.java
@@ -0,0 +1,28 @@
+package rushhour.afit.edu;
+
+import java.util.LinkedList;
+
+public class AragonSearch implements Search{
+	public LinkedList<Board>states;
+	
+	public AragonSearch(Board board) {
+		this.states = new LinkedList<Board>(); 
+		states.add(board);
+		board.visited = false;
+	}
+
+	@Override
+	public Move findMoves() {
+		Board board = states.pop();
+		Move possibleMoves = board.genMoves();
+		System.out.println(possibleMoves);
+		return null;
+	}
+
+	@Override
+	public long nodeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/rushhour/afit/edu/Move.java
@@ -0,0 +1,48 @@
+package rushhour.afit.edu;
+/**
+ * <p>Title: Rush Hour</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2005</p>
+ * <p>Company: AFIT</p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class Move
+{
+    public static final int NORTH = 1;
+    public static final int SOUTH = 2;
+    public static final int EAST = 3;
+    public static final int WEST = 4;
+
+    //type/direction flags
+    public int direction;
+    public int piece_index;
+
+    //How many squares to move
+    public int spaces;
+
+    public Move next;
+
+    public Move ( int index, int dir, int sp , Move pointer ) {
+      direction = dir;
+      piece_index = index;
+      spaces = sp;
+      next = pointer;
+    }
+    
+    public Move (Move m) {
+      direction = m.direction;
+      piece_index = m.piece_index;
+      spaces = m.spaces;
+      next = null; 
+    }
+    
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+    	String str = new String("Move: Piece " + this.piece_index + ", Direction " + this.direction + 
+    			", Spaces " + this.spaces);
+    	return str;
+    }
+}
