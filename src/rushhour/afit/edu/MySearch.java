3c1b8c5dce51fd5b3fc7706414f01d8161a51efe
7high <angelito.aragon.jr@gmail.com>
2018-01-09 19:43:33 -0500

    Implemented goaltest method

--- src/rushhour/afit/edu/MySearch.java
+++ src/rushhour/afit/edu/MySearch.java
@@ -4,16 +4,19 @@ import java.util.LinkedList;
 
 public class MySearch implements Search{
 	public LinkedList<Board>states;
+	int moveCount;
 	
 	public MySearch(Board board) {
 		this.states = new LinkedList<Board>(); 
 		states.add(board);
 		board.visited = false;
+		this.moveCount = 0;
+		System.out.println(goalTest(board));
 	}
 
 	@Override
 	public Move findMoves() {
-		Board board = states.pop();
+		Board board = states.remove();
 		Move possibleMoves = board.genMoves();
 		System.out.println(possibleMoves);
 		return null;
@@ -23,6 +26,16 @@ public class MySearch implements Search{
 	public long nodeCount() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public boolean goalTest(Board board) {		
+		Piece myCar= board.piece_list[board.findPiece("X0")];
+		
+		if ((myCar.x == Board.BOARD_EXIT_X) && (myCar.y == Board.BOARD_EXIT_Y)) {
+			return true;
+		}
+		
+		return false;
 	}
 
 }
